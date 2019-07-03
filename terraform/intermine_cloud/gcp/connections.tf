a927cb756173ef4ef4e0add77332e0523dce4dc5
Ankur Kumar <ank@leoank.me>
2019-07-03 11:01:25 +0530

    Add tf remote backend & upgrade google provider

--- terraform/intermine_cloud/gcp/connections.tf
+++ terraform/intermine_cloud/gcp/connections.tf
@@ -1,13 +1,24 @@
 provider "google" {
-    version = "~> 2.7.0"
+    version = "~> 2.9.0"
     credentials = "${file(var.credentials_file_path)}"
     project     = "${var.project_id}"
     region      = "${var.project_region}"
 }
 
 provider "google-beta" {
-    version = "~> 2.7.0"
+    version = "~> 2.9.0"
     credentials = "${file(var.credentials_file_path)}"
     project     = "${var.project_id}"
     region      = "${var.project_region}"
+}
+
+terraform {
+    backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "InterMine"
+
+    workspaces {
+        name = "intermine_cloud"
+    }
+    }
 }
