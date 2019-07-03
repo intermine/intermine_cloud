f7aed83369d19530027a95722a02bd6fe4ef55ec
Ankur Kumar <ank@leoank.me>
2019-07-03 11:02:07 +0530

    Add vars to make this module more configurable

--- terraform/intermine_cloud/gcp/variables.tf
+++ terraform/intermine_cloud/gcp/variables.tf
@@ -20,6 +20,10 @@ variable "project_region" {
   description = "Region of google cloud project"
 }
 
+variable "compute_engine_service_account" {
+  description = "Service account to associate to the nodes in the cluster"
+}
+
 variable "cluster_name" {
   description = "cluster name"
   default     = "intermine_cloud"
@@ -50,3 +54,92 @@ variable "ip_range_pods" {
 variable "ip_range_services" {
   description = "The secondary ip range to use for pods"
 }
+
+variable "http_load_balancing_boolean" {
+  type = "string"
+  description = "http load balancing boolean for kubernetes cluster"
+  default = "false"
+}
+
+variable "horizontal_pod_autoscaling_boolean" {
+  type = "string"
+  description = "horizontal pod autoscaling boolean for kubernetes cluster"
+  default = "true"
+}
+
+variable "kubernetes_dashboard_boolean" {
+  type = "string"
+  description = "kubernetes dashboard boolean for kubernetes cluster"
+  default = "true"
+}
+
+variable "network_policy_boolean" {
+  type = "string"
+  description = "network policy boolean for kubernetes cluster"
+  default = "true"
+}
+
+variable "node_pool_name" {
+  type = "string"
+  description = "node pool name"
+  default = "default-node-pool"
+}
+variable "node_pool_machine_type" {
+  type = "string"
+  description = "machine type for kubernetes nodes"
+  default = "n1-standard-1"
+}
+
+variable "node_pool_min_node_count" {
+  type = "string"
+  description = "minimum number of nodes for autoscaling"
+  default = 3
+}
+
+variable "node_pool_max_node_count" {
+  type = "string"
+  description = "maximum number of nodes for autoscaling"
+  default = 5
+}
+
+variable "node_pool_initial_node_count" {
+  type = "string"
+  description = "initial number of nodes for autoscaling"
+  default = 3
+}
+variable "node_disk_size_gb" {
+  type = "string"
+  description = "disk size in gb for nodes"
+  default = 25
+}
+
+variable "node_disk_type" {
+  type = "string"
+  description = "disk type for nodes"
+  default = "pd-standard"
+}
+
+variable "node_image_type" {
+  type = "string"
+  description = "image type for nodes"
+  default = "COS"
+}
+
+variable "node_auto_repair_boolean" {
+  type = "string"
+  description = "auto repair boolean for nodes"
+  default = "true"
+}
+
+variable "node_auto_upgrade_boolean" {
+  type = "string"
+  description = "auto upgrade boolean for nodes"
+  default = "true"
+}
+
+variable "node_preemptible_boolean" {
+  type = "string"
+  description = "preemptible boolean for nodes"
+  default = "false"
+}
+
