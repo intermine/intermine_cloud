e7e91bea3df756a46dc68d770535b7aa4700d099
Ankur Kumar <ank@leoank.me>
2019-07-30 19:13:49 +0530

    Add version declaration in module

--- terraform/intermine_cloud/gcp/kubernetes.tf
+++ terraform/intermine_cloud/gcp/kubernetes.tf
@@ -1,5 +1,6 @@
 module "gke" {
   source                     = "terraform-google-modules/kubernetes-engine/google"
+  version                    = "3.0.0"
   project_id                 = "${var.project_id}"
   name                       = "${var.cluster_name}"
   region                     = "${var.project_region}"
