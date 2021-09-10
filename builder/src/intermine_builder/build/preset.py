da16d82afd2d74850a3d34da5372841beca0cf6b
uosl <regen@anche.no>
2021-09-10 10:55:34 +0100

    Working preset mine without additional data sources

--- builder/src/intermine_builder/build/preset.py
+++ builder/src/intermine_builder/build/preset.py
@@ -20,13 +20,16 @@ def main() -> None:
         Path.home() / ".intermine" / (config["mine"] + ".properties"), properties
     )
 
-    for source in config["datasources"]:
-        logger.info("Adding data source: " + source["name"])
-        project_xml.append_source(source)
+    if "datasources" in config:
+        for source in config["datasources"]:
+            logger.info("Adding data source: " + source["name"])
+            project_xml.append_source(source)
 
-    for source in config["datasources"]:
-        gradle.integrate(source["name"])
+        for source in config["datasources"]:
+            gradle.integrate(source["name"])
 
     gradle.post_process()
+
+    gradle.build_user_db()
 
     gradle.deploy()
