a21b2d175f2c72e1e55e4a923252f9357f7dd291
uosl <regen@anche.no>
2021-09-16 17:33:00 +0100

    Working builder cli with simpler approach

--- builder/src/intermine_builder/cli.py
+++ builder/src/intermine_builder/cli.py
@@ -4,56 +4,63 @@ import click
 
 from intermine_builder import MineBuilder
 
-# TODO make one click command for each? Or try to collect as many in one command function as practical?
+# There really is no (clean) way to have both separate commands for the methods
+# that need to be handled differently, and dynamic resolution of method names.
+# This means we can't leverage option validation for each command, and need to
+# do it manually. Trust us; we tried hard.
 
-
-@click.command('create_properties_file')
-@click.argument('mine')
-@click.option('--overrides-properties')  # TODO .properties file to override defaults
-def create_properties_file(**options):
-    return
-
-
-@click.command('add_data_source')
-@click.argument('mine')
-@click.option('--name', required=True)
-@click.option('--type', required=True)
-@click.option('--property', required=True, multiple=True)
-# --properties 'src.data.dir=/data/panther,panther.organisms=7227 6239 9606'
-# --properties 'name=src.data.dir,location=/data/panther;name=panther.organisms,value=7227 6239 9606'
-# --property 'name=src.data.dir,location=/data/panther'
-# --property 'name=panther.organisms,value=7227 6239 9606'
-def add_data_source(**options):
-    click.echo(options['property'])
-    # builder = MineBuilder(options['mine'])
-    # if 'properties' in options:
-    #     options['properties'].split(',')
+BUILDER_CONSTRUCTOR_OPTIONS = ["build_image", "data_path"]
 
 
 @click.command()
-# @click.group(invoke_without_command=True)
-# @click.argument('all_args', nargs=-1)
-@click.argument('mine')
-@click.argument('task')
-@click.option('--build-image', is_flag=True)
-@click.option('--data-path', type=click.Path(exists=True))
-@click.option('--source')
-@click.option('--action')
-@click.option('--stacktrace', is_flag=True)
-@click.pass_context
-def main(ctx, **options):
-    if options['task'] == 'add_data_source':
-        return add_data_source.invoke(ctx)
-    return
-
-    builder = MineBuilder(options['mine'])
+@click.argument("mine")
+@click.argument("task")
+# Constructor
+@click.option("--build-image", is_flag=True)
+@click.option("--data-path", type=click.Path(exists=True))
+# integrate method
+@click.option("--source")
+@click.option("--action")
+# create_properties_file method
+@click.option("--overrides-properties")
+# add_data_source method
+@click.option("--name")
+@click.option("--type")
+@click.option("--property", multiple=True)
+# --property 'name=src.data.dir,location=/data/panther'
+# --property 'name=panther.organisms,value=7227 6239 9606'
+# methods that use gradle
+@click.option("--stacktrace", is_flag=True)
+def main(mine, task, **options):
+    builder_options = {k: options[k] for k in BUILDER_CONSTRUCTOR_OPTIONS}
+    builder = MineBuilder(mine, **builder_options)
     try:
-        method = getattr(builder, options['task'])
+        method = getattr(builder, task)
     except AttributeError:
-        click.echo("No task named: " + options['task'], err=True)
+        click.echo("No task named: " + task, err=True)
+        return
 
-    method(**options)
-
-
-# main.add_command(create_properties_file)
-# main.add_command(add_data_source)
+    if task == "create_properties_file":
+        # TODO parse options['overrides_properties'] which is path to .properties file
+        click.echo(task + " task is not implemented yet", err=True)
+    elif task == "add_data_source":
+        if not options["name"] or not options["type"]:
+            click.echo(task + " task requires --name and --type", err=True)
+            return
+        props = (
+            [
+                dict([kv.split("=") for kv in prop.split(",")])
+                for prop in options["property"]
+            ]
+            if options["property"]
+            else []
+        )
+        source = {"name": options["name"], "type": options["type"], "properties": props}
+        method(source)
+    elif task == "integrate":
+        if not options["source"]:
+            click.echo(task + " task requires --source", err=True)
+            return
+        method(options["source"], **options)
+    else:
+        method(**options)
