634af188a904716ed7728575866eead2868a0855
Ankur Kumar <ank@leoank.me>
2021-10-22 11:00:09 +0100

    Add rendered template API
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ compose/src/compose/schemas/api/rendered_template/__init__.py
@@ -0,0 +1,1 @@
+"""RenderedTemplate API schemas."""
---
+++ compose/src/compose/schemas/api/rendered_template/get.py
@@ -0,0 +1,34 @@
+"""RenderedTemplate API GET schema."""
+
+from enum import Enum, unique
+from typing import List, Optional
+
+from blackcap.schemas.api.common import ResponseSchema
+from compose.schemas.template import RenderedTemplate
+
+from pydantic import BaseModel
+
+
+class RenderedTemplateGetResponse(ResponseSchema):
+    """RenderedTemplate GET response schema."""
+
+    items: List[RenderedTemplate] = []
+
+
+@unique
+class RenderedTemplateQueryType(Enum):
+    """RenderedTemplate Query type enum."""
+
+    GET_ALL_RENDERED_TEMPLATES = "get_all_rendered_templates"
+    GET_RENDERED_TEMPLATE_BY_ID = "get_rendered_template_by_id"
+    GET_RENDERED_TEMPLATES_BY_PROTAGONIST_ID = (
+        "get_rendered_templates_by_protagonist_id"
+    )
+
+
+class RenderedTemplateGetQueryParams(BaseModel):
+    """RenderedTemplate GET request query params schema."""
+
+    query_type: RenderedTemplateQueryType
+    template_id: Optional[str]
+    protagonist_id: Optional[str]
