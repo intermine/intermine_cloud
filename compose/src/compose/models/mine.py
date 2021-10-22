22e512d4695c3f3199eb8aafb89461ba202534d3
Ankur Kumar <ank@leoank.me>
2021-10-22 10:58:45 +0100

    add mine model
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ compose/src/compose/models/mine.py
@@ -0,0 +1,20 @@
+"""MineDB model"""
+
+from blackcap.models.meta.mixins import DBModel, SurrogatePKUUID, TimestampMixin
+from blackcap.models.meta.orm import reference_col
+from blackcap.models.meta.helpers import GUID
+
+from sqlalchemy import Column, String, JSON, ARRAY
+
+
+class MineDB(DBModel, SurrogatePKUUID, TimestampMixin):
+    """Mine table."""
+
+    __tablename__ = "mine"
+    name = Column(String, nullable=False)
+    description = Column(String)
+    preference = Column(JSON, nullable=False)
+    state = Column(JSON, nullable=False)
+    protagonist_id = reference_col("protagonist")
+    rendered_template_id = reference_col("rendered_template_id")
+    data_file_ids = Column(ARRAY(String))
