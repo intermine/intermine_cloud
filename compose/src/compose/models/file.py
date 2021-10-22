5b9cf1e9b020903e9c46f739c47b122089c6b965
Ankur Kumar <ank@leoank.me>
2021-10-22 10:54:47 +0100

    Add file model
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ compose/src/compose/models/file.py
@@ -0,0 +1,20 @@
+"""FileDB model"""
+
+from blackcap.models.meta.mixins import DBModel, SurrogatePKUUID, TimestampMixin
+from blackcap.models.meta.helpers import GUID
+from blackcap.models.meta.orm import reference_col
+
+
+from sqlalchemy import Column, String, Boolean
+
+
+class FileDB(DBModel, SurrogatePKUUID, TimestampMixin):
+    """File table."""
+
+    __tablename__ = "file"
+    name = Column(String, nullable=False)
+    ext = Column(String)
+    file_type = Column(String, nullable=False)
+    uploaded = Column(Boolean, nullable=False)
+    parent_id = Column(GUID, nullable=False)
+    protagonist_id = reference_col("protagonist")
