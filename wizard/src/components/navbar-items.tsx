841cfc2e709784109c50ec6168a823f1d77d6a9e
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-10-26 17:19:30 +0100

    add navbar

---
+++ wizard/src/components/navbar-items.tsx
@@ -0,0 +1,128 @@
+import { useContext, useRef, useState } from 'react'
+import { useOutsideClick } from '@intermine/chromatin/utils'
+import { IconButton } from '@intermine/chromatin/icon-button'
+import { Menu } from '@intermine/chromatin/menu'
+import { MenuItem } from '@intermine/chromatin/menu-item'
+import { Box } from '@intermine/chromatin/box'
+import { Tooltip } from '@intermine/chromatin/tooltip'
+import { Divider } from '@intermine/chromatin/divider'
+import { Typography } from '@intermine/chromatin/typography'
+import DarkThemeIcon from '@intermine/chromatin/icons/Weather/sun-fill'
+import LightThemeIcon from '@intermine/chromatin/icons/Weather/moon-fill'
+
+import { AppContext } from '../context'
+import { AuthStates } from '../constants/auth'
+
+export const NavbarItems = () => {
+    const store = useContext(AppContext)
+    const {
+        preferencesReducer: {
+            state: { themeType },
+            updateTheme
+        },
+        authReducer: {
+            state: { authState, userDetails },
+            updateAuthState
+        }
+    } = store
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [anchorElement, setAnchorElement] = useState<HTMLElement>()
+
+    const menuRef = useRef(null)
+
+    const handleToggleOnClick = () => {
+        updateTheme(themeType === 'dark' ? 'light' : 'dark')
+    }
+
+    useOutsideClick({
+        anchorElement: anchorElement,
+        otherElements: [menuRef.current],
+        onInsideClicked: () => setIsMenuOpen(true),
+        onOutsideClicked: () => setIsMenuOpen(false)
+    })
+
+    if (authState === AuthStates.Authorize) {
+        return (
+            <IconButton
+                ref={(el: HTMLElement) => setAnchorElement(el)}
+                color="primary"
+                variant="normal"
+                hasElevation={false}
+                hasHighlightOnFocus={false}
+                Icon={<Box isContentCenter>Z</Box>}
+            >
+                <Menu
+                    ref={menuRef}
+                    anchorElement={anchorElement}
+                    isOpen={isMenuOpen}
+                    csx={{
+                        root: {
+                            borderRadius: '0.5rem',
+                            overflow: 'hidden'
+                        }
+                    }}
+                >
+                    <MenuItem
+                        isButtonLike={false}
+                        csx={{
+                            root: {
+                                flexDirection: 'column',
+                                alignItems: 'flex-start'
+                            }
+                        }}
+                    >
+                        <Typography variant="h3">User's name</Typography>
+                        <Typography
+                            csx={{
+                                root: ({ palette: { neutral } }) => ({
+                                    color: neutral[60]
+                                })
+                            }}
+                            variant="caption"
+                        >
+                            User's organization
+                        </Typography>
+                    </MenuItem>
+                    <Divider Component="li" csx={{ root: { margin: 0 } }} />
+                    <MenuItem>Edit Profile</MenuItem>
+                    <MenuItem
+                        onClick={() => {
+                            setIsMenuOpen(false)
+                            updateAuthState(AuthStates.NotAuthorize)
+                        }}
+                    >
+                        Logout
+                    </MenuItem>
+                    <Divider Component="li" csx={{ root: { margin: 0 } }} />
+                    <MenuItem onClick={handleToggleOnClick}>
+                        Dark Theme: {themeType === 'dark' ? 'On' : 'Off'}
+                    </MenuItem>
+                </Menu>
+            </IconButton>
+        )
+    }
+
+    return (
+        <Tooltip
+            message={`Switch to ${
+                themeType === 'dark' ? 'light' : 'dark'
+            } theme.`}
+            placement="left"
+        >
+            <IconButton
+                onClick={handleToggleOnClick}
+                color="neutral"
+                hasHighlightOnFocus={isMenuOpen ? false : true}
+                isHovered={isMenuOpen}
+                Icon={
+                    themeType === 'dark' ? (
+                        <DarkThemeIcon />
+                    ) : (
+                        <LightThemeIcon />
+                    )
+                }
+            />
+        </Tooltip>
+    )
+}
--- wizard/src/components/navbar.tsx
+++ wizard/src/components/navbar.tsx
@@ -1,17 +1,9 @@
-import { useContext, useRef, useState } from 'react'
 import { createStyle } from '@intermine/chromatin/styles'
 import { Box } from '@intermine/chromatin/box'
-import { Toggle } from '@intermine/chromatin/toggle'
 import { Typography } from '@intermine/chromatin/typography'
-import { IconButton } from '@intermine/chromatin/icon-button'
-import { Menu } from '@intermine/chromatin/menu'
-import { MenuItem } from '@intermine/chromatin/menu-item'
-import { FormControlLabel } from '@intermine/chromatin/form-control-label'
-import SettingIcon from '@intermine/chromatin/icons/System/settings-4-fill'
-import { useOutsideClick } from '@intermine/chromatin/utils'
 
-import { AppContext } from '../context'
 import InterMineLogo from '../../public/assets/img/intermine-logo.webp'
+import { NavbarItems } from './navbar-items'
 
 const useStyles = createStyle((theme) => {
     const {
@@ -47,29 +39,6 @@ const useStyles = createStyle((theme) => {
 
 export const Navbar = () => {
     const classes = useStyles()
-    const store = useContext(AppContext)
-    const {
-        state: { themeType },
-        updateTheme
-    } = store.preferencesReducer
-
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
-    const [anchorElement, setAnchorElement] = useState<
-        undefined | HTMLElement
-    >()
-
-    const menuRef = useRef(null)
-
-    const handleToggleOnClick = () => {
-        updateTheme(themeType === 'dark' ? 'light' : 'dark')
-    }
-
-    useOutsideClick({
-        anchorElement: anchorElement,
-        otherElements: [menuRef.current],
-        onInsideClicked: () => setIsMenuOpen(true),
-        onOutsideClicked: () => setIsMenuOpen(false)
-    })
 
     return (
         <Box className={classes.root}>
@@ -87,48 +56,9 @@ export const Navbar = () => {
                     Wizard
                 </Typography>
             </Box>
-            <Box>
-                <IconButton
-                    handle
-                    innerRef={(el: HTMLElement) => setAnchorElement(el)}
-                    color="neutral"
-                    hasHighlightOnFocus={isMenuOpen ? false : true}
-                    isHovered={isMenuOpen}
-                    Icon={<SettingIcon />}
-                />
+            <Box isContentAlignCenter>
+                <NavbarItems />
             </Box>
-            <Menu
-                innerRef={menuRef}
-                anchorElement={anchorElement}
-                isOpen={isMenuOpen}
-                csx={{ root: { borderRadius: '0.5rem', overflow: 'hidden' } }}
-            >
-                <MenuItem hasPadding={false}>
-                    <FormControlLabel
-                        label="Dark Theme"
-                        spaceBetween="all"
-                        csx={{
-                            root: {
-                                cursor: 'pointer',
-                                flex: '1',
-                                margin: 0,
-                                padding: '0.7rem 1.4rem'
-                            }
-                        }}
-                        labelPlacement="left"
-                        control={
-                            <Toggle
-                                isDense
-                                isChecked={themeType === 'dark'}
-                                onChange={handleToggleOnClick}
-                                hasHighlightOnFocus={false}
-                                csx={{ root: { padding: 0 } }}
-                            />
-                        }
-                    />
-                </MenuItem>
-                <MenuItem>Sign Out</MenuItem>
-            </Menu>
         </Box>
     )
 }
