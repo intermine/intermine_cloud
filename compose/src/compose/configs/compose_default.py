0891bc949f9b198d972d9fab28e540bde2f12415
Ankur Kumar <ank@leoank.me>
2021-10-27 16:34:13 +0100

    update config params
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- compose/src/compose/configs/compose_default.py
+++ compose/src/compose/configs/compose_default.py
@@ -2,6 +2,8 @@
 
 from blackcap.configs.default import DefaultConfig
 
+from xdg import xdg_data_home
+
 
 class ComposeDefaultConfig(DefaultConfig):
     """Compose default config."""
@@ -9,3 +11,6 @@ class ComposeDefaultConfig(DefaultConfig):
     MINIO_ENDPOINT: str = "localhost:1402"
     MINIO_ACCESS_KEY: str = "minioaccess"
     MINIO_SECRET_KEY: str = "minioaccess"
+    MINIO_SECURE: bool = False
+    DB_NAME: str = "compose"
+    DB_URI: str = f"sqlite:////{xdg_data_home() / ('imcloud') / ('compose.db')}"
