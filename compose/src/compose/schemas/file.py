bb9b424e1a469624ec0fc332b41e66b5c116a294
Ankur Kumar <ank@leoank.me>
2021-10-27 16:34:42 +0100

    update file schema
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- compose/src/compose/schemas/file.py
+++ compose/src/compose/schemas/file.py
@@ -1,6 +1,7 @@
 """"File schema."""
 
 from typing import Dict, Optional
+from uuid import uuid4
 
 from pydantic import BaseModel
 from pydantic.types import UUID4
@@ -9,11 +10,10 @@ from pydantic.types import UUID4
 class File(BaseModel):
     """File schema."""
 
-    file_id: Optional[UUID4]
+    file_id: Optional[UUID4] = uuid4()
     name: str
     ext: Optional[str]
     file_type: str
     parent_id: UUID4
-    presigned_post: Optional[str]
-    presigned_form: Optional[Dict]
+    presigned_url: Optional[str]
     uploaded: bool = False
