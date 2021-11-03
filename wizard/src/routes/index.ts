5fb3e9fcc13c88612bd41e4fac867c5fc84fd1d9
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-11-03 17:03:44 +0000

    small changes

--- wizard/src/routes/index.ts
+++ wizard/src/routes/index.ts
@@ -10,5 +10,6 @@ export const FORGOT_PASSWORD_PATH = `${PRE_AUTH_PATH_BASE}/forgot-password`
 export const AUTH_PATH_BASE = `${BASE_PATH}/app`
 export const DASHBOARD_OVERVIEW_PATH = `${AUTH_PATH_BASE}/overview`
 export const DASHBOARD_DATA_PATH = `${AUTH_PATH_BASE}/data`
+export const DASHBOARD_MINES_PATH = `${AUTH_PATH_BASE}/mines`
 
 export { isAuthRoute } from './validate'
