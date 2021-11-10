e10c6faf9d4cdbb8895567b6ca1245dcc2267428
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-11-10 12:51:54 +0000

    refactor additional sidebar

--- wizard/src/domain/dashboard/additional-sidebar/edit-profile.tsx
+++ wizard/src/domain/dashboard/additional-sidebar/edit-profile.tsx
@@ -1,5 +1,22 @@
+import { Input, IconButton, Box } from '@intermine/chromatin'
+import CheckIcon from '@intermine/chromatin/icons/System/check-line'
+
 import { ActionSection } from './action-section'
 
 export const EditProfile = () => {
-    return <ActionSection heading="Edit Profile" />
+    return (
+        <ActionSection heading="Edit Profile">
+            <ActionSection.Content>
+                <Box isContentCenter>
+                    <Input RightIcon={<CheckIcon />} />
+                    {/* <IconButton
+                        csx={{ root: { borderRadius: '0' } }}
+                        variant="normal"
+                        color="neutral"
+                        Icon={<CheckIcon />}
+                    /> */}
+                </Box>
+            </ActionSection.Content>
+        </ActionSection>
+    )
 }
