880e550ee97eba93b132537922fcb7d7f58c7984
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-11-16 17:52:27 +0000

    update progress tab view

--- wizard/src/domain/dashboard/additional-sidebar/progress.tsx
+++ wizard/src/domain/dashboard/additional-sidebar/progress.tsx
@@ -1,20 +1,50 @@
-import { AdditionalSidebarTabs } from '../../../constants/additional-sidebar'
+import { Typography } from '@intermine/chromatin/typography'
 
-import { useAdditionalSidebarReducer } from '../../../context'
+import { AdditionalSidebarTabs } from '../../../constants/additional-sidebar'
+import {
+    useAdditionalSidebarReducer,
+    useProgressReducer
+} from '../../../context'
 import { ActionSection } from './action-section'
+import { ProgressItemView } from './progress-item-view'
 
 export const Progress = () => {
     const additionalSidebarReducer = useAdditionalSidebarReducer()
+    const progressReducer = useProgressReducer()
+
     const {
         state: { activeTab }
     } = additionalSidebarReducer
+
+    const {
+        state: { progressItems },
+        stopDataUploading,
+        removeEntry,
+        retryUpload
+    } = progressReducer
 
     return (
         <ActionSection
             heading="Progress"
             isActive={activeTab === AdditionalSidebarTabs.ProgressTab}
         >
-            Progress Section
+            <ActionSection.Content>
+                {Object.keys(progressItems).length === 0 && (
+                    <Typography color="neutral.30">
+                        Nothing is in progress.
+                    </Typography>
+                )}
+
+                {Object.keys(progressItems).map((key) => (
+                    <ProgressItemView
+                        key={progressItems[key].id}
+                        onCancelUpload={stopDataUploading}
+                        onRemoveClick={removeEntry}
+                        onRetryClick={retryUpload}
+                        {...progressItems[key]}
+                    />
+                ))}
+            </ActionSection.Content>
         </ActionSection>
     )
 }
