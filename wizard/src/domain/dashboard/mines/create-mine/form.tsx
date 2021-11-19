7918e68fa4d6627097c426d8b0166252b79903c9
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-11-19 18:48:22 +0000

    add create mine inputs

--- wizard/src/domain/dashboard/mines/create-mine/form.tsx
+++ wizard/src/domain/dashboard/mines/create-mine/form.tsx
@@ -1,21 +1,40 @@
-import { Select } from '@intermine/chromatin/select'
 import { Box } from '@intermine/chromatin/box'
 import { createStyle } from '@intermine/chromatin/styles'
 import { Label } from '@intermine/chromatin/label'
+import { Input, InputProps } from '@intermine/chromatin/input'
 import {
     Typography,
     TypographyBaseProps
 } from '@intermine/chromatin/typography'
+import { FormSelect } from '../../../../components/form/form-select'
+import { Button } from '@intermine/chromatin'
+import clsx from 'clsx'
 
-const dummySelectOptions = [
+const dummyTemplateOptions = [
     { label: 'HumanMine', value: 'template-1' },
     { label: 'CovidMine', value: 'template-2' },
     { label: 'FlyMine', value: 'template-3' },
     { label: 'RatMine', value: 'template-4' }
 ]
 
+const dummyDatasetsOptions = [
+    { label: 'Dataset 1', value: 'template-1' },
+    { label: 'Dataset 2', value: 'template-2' },
+    { label: 'Dataset 3', value: 'template-3' },
+    { label: 'Dataset 4', value: 'template-4' },
+    { label: 'Dataset 5', value: 'template-5' },
+    { label: 'Dataset 6', value: 'template-6' },
+    { label: 'Dataset 7', value: 'template-7' },
+    { label: 'Dataset 8', value: 'template-8' },
+    { label: 'Dataset 9', value: 'template-9' },
+    { label: 'Dataset 10', value: 'template-10' }
+]
+
 const useStyles = createStyle((theme) => {
-    const { spacing } = theme
+    const {
+        spacing,
+        palette: { neutral, grey, themeType }
+    } = theme
 
     return {
         form: {
@@ -37,12 +56,44 @@ const useStyles = createStyle((theme) => {
             margin: spacing(0, 0, 8, 0),
             maxWidth: '35rem',
             width: '100%'
+        },
+        subdomainInput: {
+            '&&': {
+                borderRadius: '0.25rem 0 0 0.25rem',
+                textAlign: 'right'
+            }
+        },
+        intermineBox: {
+            flex: 1,
+            background: themeType === 'dark' ? neutral[10] : grey[20],
+            borderRadius: '0 0.25rem 0.25rem 0',
+            padding: spacing(1, 2, 1, 1)
+        },
+        actionContainer: {
+            display: 'flex',
+            justifyContent: 'flex-end',
+            paddingTop: spacing(20)
         }
     }
 })
 
 const FormTypography = (props: TypographyBaseProps) => {
     return <Typography csx={{ root: { fontWeight: 100 } }} {...props} />
+}
+
+const FormInput = (props: InputProps) => {
+    return (
+        <Input
+            hasFullWidth
+            hasTransparentBackground
+            csx={{
+                inputRoot: {
+                    ':hover': { background: 'transparent' }
+                }
+            }}
+            {...props}
+        />
+    )
 }
 
 export const Form = () => {
@@ -61,7 +112,7 @@ export const Form = () => {
                             Make sure you have already uploaded the template.
                             There are also some of the pre-existing templates.
                         </FormTypography>
-                        <Select options={dummySelectOptions} />
+                        <FormSelect options={dummyTemplateOptions} />
                     </Label>
                     <Label className={classes.formGroup}>
                         <FormTypography variant="h3">
@@ -71,8 +122,76 @@ export const Form = () => {
                             Make sure you have already uploaded the dataset. You
                             can select multiple dataset.
                         </FormTypography>
-                        <Select options={dummySelectOptions} isMulti />
+                        <FormSelect
+                            closeMenuOnSelect={false}
+                            options={dummyDatasetsOptions}
+                            isMulti
+                        />
                     </Label>
+                    <Label className={classes.formGroup}>
+                        <FormTypography variant="h3">
+                            Name your new Mine
+                        </FormTypography>
+                        <FormTypography variant="body" color="neutral.20">
+                            This will be the name under which your mine is
+                            publicly available. Some examples are HumanMine,
+                            FlyMine, CovidMine, etc.
+                        </FormTypography>
+                        <FormInput placeholder="Enter mine name" />
+                    </Label>
+                    <Label className={classes.formGroup}>
+                        <FormTypography variant="h3">
+                            Description of your Mine
+                        </FormTypography>
+                        <FormTypography variant="body" color="neutral.20">
+                            This will help other users to get an idea about your
+                            mine. You can write something like: An integrated
+                            data warehouse for...
+                        </FormTypography>
+                        <FormInput
+                            rows={5}
+                            Component="textarea"
+                            placeholder="Description of your mine"
+                        />
+                    </Label>
+                    <Label className={classes.formGroup}>
+                        <FormTypography variant="h3">Sub Domain</FormTypography>
+                        <FormTypography variant="body" color="neutral.20">
+                            We will host your newly built mine under this
+                            sub-domain. Please don't include any special
+                            character.
+                        </FormTypography>
+                        <Box display="flex">
+                            <FormInput
+                                classes={{ inputRoot: classes.subdomainInput }}
+                                placeholder="my-first-intermine-database"
+                            />
+                            <Box
+                                isContentCenter
+                                className={classes.intermineBox}
+                            >
+                                .intermine.org
+                            </Box>
+                        </Box>
+                    </Label>
+                    <Box
+                        className={clsx(
+                            classes.actionContainer,
+                            classes.formGroup
+                        )}
+                    >
+                        <Button
+                            color="warning"
+                            csx={{
+                                root: ({ spacing }) => ({
+                                    marginRight: spacing(10)
+                                })
+                            }}
+                        >
+                            Reset
+                        </Button>
+                        <Button color="primary">Create Mine</Button>
+                    </Box>
                 </Box>
             </Box>
         </form>
