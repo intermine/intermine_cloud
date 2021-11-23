803aecc2f19b6c9ee8d2293d1e3f10b70aa8cdb5
Ankur Kumar <ank@leoank.me>
2021-11-23 18:44:31 +0000

    Add check authentication decorator function
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- compose/src/compose/schemas/api/auth/put.py
+++ compose/src/compose/schemas/api/auth/put.py
@@ -1,6 +1,7 @@
 """Auth API PUT schema."""
-from typing import Optional
+from typing import List, Optional
 
+from blackcap.schemas.user import User
 from pydantic import BaseModel
 
 
@@ -10,3 +11,9 @@ class AuthPUTRequest(BaseModel):
     email: str
     password_reset_token: Optional[str]
     new_password: Optional[str]
+
+
+class AuthPUTResponse(BaseModel):
+    """Auth POST response schema."""
+
+    items: List[User] = []
