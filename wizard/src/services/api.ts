54e7e9f4c5750bb01f9867460d52133a2014bbeb
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-12-20 15:11:43 +0000

    export all api with configuration

--- wizard/src/services/api.ts
+++ wizard/src/services/api.ts
@@ -1,6 +1,19 @@
-import { AuthApi, UserApi } from '@intermine/compose-rest-client'
+import {
+    AuthApi,
+    UserApi,
+    DataApi,
+    FileApi,
+    MineApi,
+    JobApi,
+    TemplateApi,
+} from '@intermine/compose-rest-client'
 
 import { composeConfiguration } from '../config'
 
 export const authApi = new AuthApi(composeConfiguration)
 export const userApi = new UserApi(composeConfiguration)
+export const dataApi = new DataApi(composeConfiguration)
+export const fileApi = new FileApi(composeConfiguration)
+export const mineApi = new MineApi(composeConfiguration)
+export const templateApi = new TemplateApi(composeConfiguration)
+export const jobApi = new JobApi(composeConfiguration)
