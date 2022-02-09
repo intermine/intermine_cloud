be35d8362447c96dcca10467c9577f08756d950a
Ankur Kumar <ank@leoank.me>
2022-02-09 15:22:34 +0000

    Add mine action blueprint
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- compose/src/compose/routes/mine/__init__.py
+++ compose/src/compose/routes/mine/__init__.py
@@ -9,5 +9,8 @@ mine_bp = Blueprint("mine", __name__, url_prefix="/v1/mine")
 # from compose.routes.mine.delete import delete  # noqa: F401, E402, I100
 from compose.routes.mine.get import get  # noqa: F401, E402, I100
 from compose.routes.mine.post import post  # noqa: F401, E402, I100
+from compose.routes.mine.action import mine_action_bp  # noqa: F401, E402, I100
 
 # from compose.routes.mine.put import put  # noqa: F401, E402, I100
+
+mine_bp.register_blueprint(mine_action_bp)
