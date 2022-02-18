59d59d171589cc912e0b2ea6a3484ca84805b4cf
Ankur Kumar <ank@leoank.me>
2022-02-18 13:16:27 +0000

    update template schema
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- compose/src/compose/models/template.py
+++ compose/src/compose/models/template.py
@@ -12,5 +12,5 @@ class TemplateDB(DBModel, SurrogatePKUUID, TimestampMixin):
     name = Column(String, nullable=False)
     description = Column(String)
     template_vars = Column(JSON)
-    latest_file_id = reference_col("file", nullable=True)
+    file_id = reference_col("file", nullable=True)
     protagonist_id = reference_col("protagonist")
