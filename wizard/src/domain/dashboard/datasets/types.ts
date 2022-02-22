d1c5a620b03fda42b0f239aa4f44e12bb8e33505
Ankit Kumar <ankits.kvmdam@hotmail.com>
2022-02-22 16:02:00 +0000

    add types

--- wizard/src/domain/dashboard/datasets/types.ts
+++ wizard/src/domain/dashboard/datasets/types.ts
@@ -1,15 +1,9 @@
+import { Model200DataResponseAllOfItems } from '@intermine/compose-rest-client'
+
 /**
  * There are other properties as well, but here
  * we are only add those properties which we want.
  */
 export type TDatasetResponseData = {
-    msg: string
-    items: Array<{
-        data_id: string
-        name: string
-        file_id: string
-        file_type: string
-        ext: string
-        description: string
-    }>
+    items: Model200DataResponseAllOfItems
 }
