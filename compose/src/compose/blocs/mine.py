81f8b494002ea7a262dcdecc26cd423186ee1d8d
Ankur Kumar <ank@leoank.me>
2022-02-25 19:08:34 +0000

    update mine blocs
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- compose/src/compose/blocs/mine.py
+++ compose/src/compose/blocs/mine.py
@@ -3,13 +3,25 @@
 from typing import Dict, List, Optional
 
 from blackcap.db import DBSession
+from blackcap.flow import Flow, FlowExecError, FuncProp, get_outer_function, Prop, Step
+from blackcap.flow.step import dummy_backward
 from blackcap.schemas.user import User
 from logzero import logger
+from pydantic import ValidationError
+from pydantic.error_wrappers import ErrorWrapper
 from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
 
+from compose.blocs.file import (
+    create_file_db_entry,
+    create_file_presigned_urls,
+    revert_file_db_entry,
+)
 from compose.models.mine import MineDB
+from compose.schemas.data import Data
 from compose.schemas.api.mine.delete import MineDelete
 from compose.schemas.api.mine.get import MineGetQueryParams, MineQueryType
+from compose.schemas.api.mine.post import MineCreate
 from compose.schemas.api.mine.put import MineUpdate
 from compose.schemas.mine import Mine
 
@@ -19,12 +31,12 @@ from compose.schemas.mine import Mine
 ######################
 
 
-def create_mine(mine_list: List[Mine], user_creds: Optional[User] = None) -> List[Mine]:
+def create_mine(mine_list: List[MineCreate], user_creds: User) -> List[Mine]:
     """Create mine objects.
 
     Args:
-        mine_list (List[Mine]): List of mine objects to create.
-        user_creds (Optional[User], optional): User credentials. Defaults to None.
+        mine_list (List[MineCreate]): List of mine objects to create.
+        user_creds (User): User credentials.
 
     Raises:
         Exception: Database error
@@ -37,7 +49,7 @@ def create_mine(mine_list: List[Mine], user_creds: Optional[User] = None) -> Lis
             mine_db_create_list: List[MineDB] = [
                 MineDB(
                     protagonist_id=user_creds.user_id,
-                    **mine.dict(exclude={"mine_id", "protagonist_id", "template_id"}),
+                    **mine.dict(),
                 )
                 for mine in mine_list
             ]
@@ -51,14 +63,13 @@ def create_mine(mine_list: List[Mine], user_creds: Optional[User] = None) -> Lis
             raise e
 
 
-def get_mine(
-    query_params: MineGetQueryParams, user_creds: Optional[User] = None
-) -> List[Mine]:
+def get_mine(query_params: MineGetQueryParams, user_creds: User) -> List[Mine]:
     """Query DB for Mines.
 
     Args:
         query_params (MineGetQueryParams): Query params from request
-        user_creds (Optional[User], optional): User credentials. Defaults to None.
+        user_creds (User): User credentials.
+
 
     Raises:
         Exception: error
@@ -69,11 +80,31 @@ def get_mine(
     stmt = ""
 
     if query_params.query_type == MineQueryType.GET_ALL_MINES:
-        stmt = select(MineDB)
+        stmt = select(MineDB).where(MineDB.protagonist_id == user_creds.user_id)
     if query_params.query_type == MineQueryType.GET_MINE_BY_ID:
-        stmt = select(MineDB).where(MineDB.id == query_params.mine_id)
+        if query_params.mine_id is None:
+            e = ValidationError(
+                errors=[
+                    ErrorWrapper(ValueError("field required"), "mine_id"),
+                ],
+                model=MineGetQueryParams,
+            )
+            raise e
+        stmt = (
+            select(MineDB)
+            .where(MineDB.protagonist_id == user_creds.user_id)
+            .where(MineDB.id == query_params.mine_id)
+        )
     if query_params.query_type == MineQueryType.GET_MINES_BY_PROTAGONIST_ID:
-        stmt = select(MineDB).where(MineDB.id == query_params.protagonist_id)
+        if query_params.mine_id is None:
+            e = ValidationError(
+                errors=[
+                    ErrorWrapper(ValueError("field required"), "protagonist_id"),
+                ],
+                model=MineGetQueryParams,
+            )
+            raise e
+        stmt = select(MineDB).where(MineDB.id == user_creds.user_id)
 
     with DBSession() as session:
         try:
@@ -85,63 +116,73 @@ def get_mine(
             raise e
 
 
-def update_mine(mine_update: MineUpdate, user_creds: Optional[User] = None) -> Mine:
+def update_mine(mine_update_list: List[MineUpdate], user_creds: User) -> List[Mine]:
     """Update Mine in the DB from MineUpdate request.
 
     Args:
-        mine_update (MineUpdate): MineUpdate request
-        user_creds (Optional[User], optional): User credentials. Defaults to None.
+        mine_update_list (List[MineUpdate]): List of MineUpdate request
+        user_creds (User): User credentials.
 
     Raises:
         Exception: error
 
     Returns:
-        Mine: Instance of Updated Mine
+        List[Mine]: List of Instance of Updated Mine
     """
-    stmt = select(MineDB).where(MineDB.id == mine_update.mine_id)
+    stmt = (
+        select(MineDB)
+        .where(MineDB.protagonist_id == user_creds.user_id)
+        .where(MineDB.id.in_([mine_update.mine_id for mine_update in mine_update_list]))
+    )
     with DBSession() as session:
         try:
-            mine_list: List[MineDB] = session.execute(stmt).scalars().all()
-            if len(mine_list) == 1:
-                mine_update_dict = mine_update.dict(exclude_defaults=True)
-                mine_update_dict.pop("mine_id")
-                updated_mine = mine_list[0].update(session, **mine_update_dict)
-                return Mine(data_id=updated_mine.id, **updated_mine.to_dict())
-            if len(mine_list) == 0:
-                # TODO: Raise not found
-                pass
+            mine_db_update_list: List[MineDB] = session.execute(stmt).scalars().all()
+            updated_mine_list = []
+            for mine in mine_db_update_list:
+                for mine_update in mine_update_list:
+                    if mine_update.mine_id == mine.id:
+                        mine_update_dict = mine_update.dict(exclude_defaults=True)
+                        mine_update_dict.pop("mine_id")
+                        updated_mine = mine.update(session, **mine_update_dict)
+                        updated_mine_list.append(
+                            Mine(mine_id=updated_mine.id, **updated_mine.to_dict())
+                        )
+            return updated_mine_list
         except Exception as e:
             session.rollback()
-            logger.error(f"Unable to update mine: {mine_update.dict()} due to {e}")
+            logger.error(f"Unable to update mine: {mine.to_dict()} due to {e}")
             raise e
 
 
-def delete_mine(mine_delete: MineDelete, user_creds: Optional[User] = None) -> Mine:
+def delete_mine(mine_delete_list: List[MineDelete], user_creds: User) -> List[Mine]:
     """Delete Mine in the DB from MineDelete request.
 
     Args:
-        mine_delete (MineDelete): MineDelete request
-        user_creds (Optional[User], optional): User credentials. Defaults to None.
+        mine_delete_list (List[MineDelete]): List of MineDelete request
+        user_creds (User): User credentials.
 
     Raises:
         Exception: error
 
     Returns:
-        Mine: Instance of Deleted Mine
+        List[Mine]: List of Instance of Deleted Mine
     """
-    stmt = select(MineDB).where(MineDB.id == mine_delete.mine_id)
+    stmt = (
+        select(MineDB)
+        .where(MineDB.protagonist_id == user_creds.user_id)
+        .where(MineDB.id.in_([mine.mine_id for mine in mine_delete_list]))
+    )
     with DBSession() as session:
         try:
-            mine_list: List[MineDB] = session.execute(stmt).scalars().all()
-            if len(mine_list) == 1:
-                deleted_mine = mine_list[0].delete(session)
-                return Mine(mine_id=deleted_mine.id, **deleted_mine.to_dict())
-            if len(mine_list) == 0:
-                # TODO: Raise not found
-                pass
+            mine_db_delete_list: List[MineDB] = session.execute(stmt).scalars().all()
+            deleted_mine_list = []
+            for mine in mine_db_delete_list:
+                mine.delete(session)
+                deleted_mine_list.append(Mine(mine_id=mine.id, **mine.to_dict()))
+            return deleted_mine_list
         except Exception as e:
             session.rollback()
-            logger.error(f"Unable to delete mine: {mine_delete.dict()} due to {e}")
+            logger.error(f"Unable to delete mine: {mine.to_dict()} due to {e}")
             raise e
 
 
@@ -149,44 +190,236 @@ def delete_mine(mine_delete: MineDelete, user_creds: Optional[User] = None) -> M
 # Flow BloCs
 ######################
 
-# TODO: Add `flow_graph` and `flow_state` data structures
-# TODO: Add `reverse_flow(flow_state)` function for each flow
 
-
-def create_mine_flow(
-    mine_list: List[Mine],
-    interface: str,
-    inteface_params: Dict,
-    user_creds: Optional[User] = None,
-) -> List[Mine]:
-    """Create mine flow.
+def check_data_list_exist(inputs: List[Prop]) -> List[Prop]:
+    """Check data list exist step.
 
     Args:
-        mine_list (List[Mine]): List of mine objects to create.
-        interface (str): Interface used to initiate the request. options include "cli" and "rest".
-        inteface_params (Dict): Parameters specific to the interface.
-        user_creds (Optional[User], optional): [description]. Defaults to None.
+        inputs (List[Prop]):
+            Expects
+                0: data_list
+                    Prop(data=data_list, description="List of data objects")
+                2: user
+                    Prop(data=user, description="User")
 
-    Returns: # noqa: DAR202
-        List[Mine]: Created mine objects
+    Raises:
+        FlowExecError: Flow execution failed
+
+    Returns:
+        List[Prop]:
+
+            Prop(data=data_list, description="List of Data Objects")
+
+            Prop(data=user, description="User")
     """
-    pass
+    try:
+        data_list: List[Data] = inputs[0].data
+        user: User = inputs[1].data
+    except Exception as e:
+        raise FlowExecError(
+            human_description="Parsing inputs failed",
+            error=e,
+            error_type=type(e),
+            is_user_facing=True,
+            error_in_function=get_outer_function(),
+        ) from e
+
+    try:
+        # TODO: Check data list existence
+        pass
+    except SQLAlchemyError as e:
+        raise FlowExecError(
+            human_description="Querying DB object failed",
+            error=e,
+            error_type=type(e),
+            is_user_facing=False,
+            error_in_function=get_outer_function(),
+        ) from e
+    except Exception as e:
+        raise FlowExecError(
+            human_description="Something bad happened",
+            error=e,
+            error_type=type(e),
+            is_user_facing=False,
+            error_in_function=get_outer_function(),
+        ) from e
+
+    return [
+        Prop(data=data_list, description="List of Data Objects"),
+        Prop(data=user, description="User"),
+    ]
 
 
-def create_mine_reverse_flow(
-    mine_list: List[Mine],
-    interface: str,
-    inteface_params: Dict,
-    flow_state: Dict,
-    user_creds: Optional[User] = None,
-) -> None:
-    """Create mine flow.
+def create_mine_db_entry(inputs: List[Prop]) -> List[Prop]:
+    """Forward function for create db entry step.
 
     Args:
-        mine_list (List[Mine]): List of mine objects to create.
-        interface (str): Interface used to initiate the request. options include "cli" and "rest".
-        inteface_params (Dict): Parameters specific to the interface.
-        flow_state (Dict): FlowState object.
-        user_creds (Optional[User], optional): [description]. Defaults to None.
+        inputs (List[Prop]):
+            Expects
+                0: mine_create_request_list
+                    Prop(data=mine_create_request_list, description="List of create mine objects")
+                2: user
+                    Prop(data=user, description="User")
+
+    Raises:
+        FlowExecError: Flow execution failed
+
+    Returns:
+        List[Prop]:
+            Created data objects
+
+            Prop(data=created_data_list, description="List of created Data Objects")
+
+            Prop(data=user, description="User")
     """
-    pass
+    try:
+        mine_create_request_list: List[MineCreate] = inputs[0].data
+        user: User = inputs[1].data
+    except Exception as e:
+        raise FlowExecError(
+            human_description="Parsing inputs failed",
+            error=e,
+            error_type=type(e),
+            is_user_facing=True,
+            error_in_function=get_outer_function(),
+        ) from e
+
+    try:
+        created_mine_list = create_mine(mine_create_request_list, user)
+    except SQLAlchemyError as e:
+        raise FlowExecError(
+            human_description="Creating DB object failed",
+            error=e,
+            error_type=type(e),
+            is_user_facing=False,
+            error_in_function=get_outer_function(),
+        ) from e
+    except Exception as e:
+        raise FlowExecError(
+            human_description="Something bad happened",
+            error=e,
+            error_type=type(e),
+            is_user_facing=False,
+            error_in_function=get_outer_function(),
+        ) from e
+
+    return [
+        Prop(data=created_mine_list, description="List of created Mine Objects"),
+        Prop(data=user, description="User"),
+    ]
+
+
+def revert_mine_db_entry(inputs: List[Prop]) -> List[Prop]:
+    """Revert function for create db entry step.
+
+    Args:
+        inputs (List[Prop]):
+            Expects
+                0: mine_create_request_list
+                    Prop(data=data_create_request_list, description="List of create data objects")
+                1: user
+                    Prop(data=user, description="User")
+                2: created_mine_list
+                    Prop(data=created_mine_list, description="List of created mine objects")
+                3: user
+                    Prop(data=user, description="User")
+
+    Raises:
+        FlowExecError: Flow execution failed
+
+    Returns:
+        List[Prop]:
+            Deleted mine objects
+
+            Prop(data=deleted_mine_list, description="List of deleted Mine Objects")
+
+            Prop(data=user, description="User")
+    """
+    try:
+        created_mine_list: List[Data] = inputs[2].data
+        user: User = inputs[3].data
+    except Exception as e:
+        raise FlowExecError(
+            human_description="Parsing inputs failed",
+            error=e,
+            error_type=type(e),
+            is_user_facing=True,
+            error_in_function=get_outer_function(),
+        ) from e
+
+    try:
+        deleted_mine_list = delete_mine(created_mine_list, user)
+    except SQLAlchemyError as e:
+        raise FlowExecError(
+            human_description="Deleting DB object failed",
+            error=e,
+            error_type=type(e),
+            is_user_facing=False,
+            error_in_function=get_outer_function(),
+        ) from e
+    except Exception as e:
+        raise FlowExecError(
+            human_description="Something bad happened",
+            error=e,
+            error_type=type(e),
+            is_user_facing=False,
+            error_in_function=get_outer_function(),
+        ) from e
+
+    return [
+        Prop(data=deleted_mine_list, description="List of deleted Mine Objects"),
+        Prop(data=user, description="User"),
+    ]
+
+
+def generate_create_mine_flow(
+    mine_create_request_list: List[MineCreate], user: User
+) -> Flow:
+    """Generate flow for creating the mine resource.
+
+    Args:
+        mine_create_request_list (List[MineCreate]): List of mine objects to create.
+        user (User): User credentials.
+
+    Returns:
+        Flow: Create data flow
+    """
+    create_db_entry_step = Step(create_mine_db_entry, revert_mine_db_entry)
+    create_file_step = Step(create_file_db_entry, revert_file_db_entry)
+    create_file_presigned_url_step = Step(create_file_presigned_urls, dummy_backward)
+    update_db_entry_step = Step(update_mine_db_entry, rewind_mine_db_entry)
+
+    flow = Flow()
+
+    flow.add_step(
+        create_db_entry_step,
+        [
+            Prop(
+                data=mine_create_request_list,
+                description="List of DataCreate Objects",
+            ),
+            Prop(data=user, description="User"),
+        ],
+    )
+    create_file_step_func_prop = FuncProp(
+        func=flow.get_froward_output,
+        params={"index": 0},
+        description="Outputs of first step",
+    )
+    create_file_presigned_url_step_func_prop = FuncProp(
+        func=flow.get_froward_output,
+        params={"index": 1},
+        description="Outputs of second step",
+    )
+    update_db_entry_step_func_prop = FuncProp(
+        func=flow.get_froward_output,
+        params={"index": 2},
+        description="Outputs of third step",
+    )
+    flow.add_step(create_file_step, [create_file_step_func_prop])
+    flow.add_step(
+        create_file_presigned_url_step, [create_file_presigned_url_step_func_prop]
+    )
+    flow.add_step(update_db_entry_step, [update_db_entry_step_func_prop])
+
+    return flow
