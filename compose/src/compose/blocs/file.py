bc5a132e3d59fc4a5653baf7c804fe8f6c91e4dd
Ankur Kumar <ank@leoank.me>
2022-03-06 17:33:37 +0000

    update file bloc
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- compose/src/compose/blocs/file.py
+++ compose/src/compose/blocs/file.py
@@ -22,7 +22,7 @@ from compose.schemas.api.file.post import FileCreate
 from compose.schemas.api.file.put import FileUpdate
 from compose.schemas.data import Data
 from compose.schemas.file import File
-from compose.schemas.template import Template
+from compose.schemas.template import RenderedTemplate, Template
 
 
 config = config_registry.get_config()
@@ -259,7 +259,9 @@ def create_file_db_entry(inputs: List[Prop]) -> List[Prop]:
             Prop(data=user, description="User")
     """
     try:
-        created_parent_list: List[Union[Data, Template]] = inputs[0].data
+        created_parent_list: List[Union[Data, Template, RenderedTemplate]] = inputs[
+            0
+        ].data
         user: User = inputs[1].data
     except Exception as e:
         raise FlowExecError(
